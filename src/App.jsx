9ab959dc4fcf01346f74f16e3d97f8af8828d4ee
shrikant <sbip0863@uni.sydney.edu.au>
2025-08-06 22:04:49 +0530

    Added basic functionality and design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,47 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState(''); // stores text in input box
+  const [tasks, setTasks] = useState([]); // stores all tasks
+
+  const addTask = () => {
+    const trimmedInput = input.trim();
+
+    if(!trimmedInput) {
+      return;   // do nothing if theres no tasks added
+    }
+
+    const newTask = {
+      id: Date.now(),   // unique id
+      text: input,
+      completed: false 
+    };
+
+    setTasks([...tasks,newTask]);
+    setInput('');
+
+  }
 
   return (
-    <div>
+    <div className="app-container">
+      <h1>Tasky</h1>
+
+      <input
+        type='text'
+        placeholder='Enter a task...'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button onClick={addTask} disabled={!input.trim()}>Add</button>
+
+      <ul className='task-list'>
+        {tasks.map(task => (
+          <li key={task.id} className='task-item'>
+            {task.text}
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
